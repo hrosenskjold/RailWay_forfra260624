41617be5c1cca2834da68d1d5530237ac7d6a329
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2024-06-28 11:06:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-import json
-from geojson import Feature, FeatureCollection
-from arcgis.geometry import Geometry
 
 app = Flask(__name__)
 
@@ -18,12 +15,9 @@ def upload_file():
     arcgis_json = convert_geojson_to_arcgis_json(geojson)
 
     # Add the GeoJSON layer to the ArcGIS Feature Service
-    response = requests.post(
-        'https://services6.arcgis.com/QHir1urgnGYroCLG/arcgis/rest/services/PG_versioneret_110624/FeatureServer/0/addFeatures',
-        json={
-            'features': arcgis_json['features']
-        }
-    )
+    response = requests.post('https://services6.arcgis.com/QHir1urgnGYroCLG/arcgis/rest/services/PG_versioneret_110624/FeatureServer/0/addFeatures', {
+        'features': json.dumps(arcgis_json)
+    })
 
     if response.status_code == 200:
         return jsonify(message='Layer successfully added'), 200
@@ -31,15 +25,12 @@ def upload_file():
         return jsonify(error='Failed to add layer to ArcGIS Feature Service'), 500
 
 def convert_geojson_to_arcgis_json(geojson):
-    features = []
-    for feature in geojson['features']:
-        geom = Geometry(feature['geometry'])
-        arcgis_feature = {
-            'geometry': geom,
-            'attributes': feature['properties']
-        }
-        features.append(arcgis_feature)
-    return {'features': features}
+    import arcgis2geojson
+
+    # Convert GeoJSON to ArcGIS JSON
+    arcgis_json = arcgis2geojson.geojson2arcgis(geojson)
+
+    return arcgis_json
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
