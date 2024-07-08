41d04d1b97131696f5e3917124efda263dc767df
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2024-07-08 11:01:49 +0200

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 from arcgis.geometry import Geometry
-from arcgis.features import Feature
+from arcgis.features import Feature, FeatureSet
 import json
 
 app = Flask(__name__)
@@ -13,32 +13,40 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     geojson = request.get_json()
+
     if not geojson:
         return jsonify(error='No GeoJSON data provided'), 400
 
-    # Convert GeoJSON to ArcGIS JSON
-    arcgis_json = convert_geojson_to_arcgis_json(geojson)
+    # Convert GeoJSON to ArcGIS Features
+    arcgis_features = convert_geojson_to_arcgis_features(geojson)
 
-    # Use proxy to add the GeoJSON layer to the ArcGIS Feature Service
-    response = proxy_request(arcgis_json)
+    # Use proxy to add the features to the ArcGIS Feature Service
+    response = proxy_request(arcgis_features)
 
     if response.status_code == 200:
         return jsonify(message='Layer successfully added'), 200
     else:
         return jsonify(error='Failed to add layer to ArcGIS Feature Service'), 500
 
-def convert_geojson_to_arcgis_json(geojson):
-    # Convert GeoJSON to ArcGIS JSON
-    arcgis_feature = Feature(geometry=Geometry(geojson))
-    arcgis_json = arcgis_feature.as_dict  # Fjern parenteserne
+def convert_geojson_to_arcgis_features(geojson):
+    features = []
+    for feature in geojson['features']:
+        geometry = Geometry(feature['geometry'])
+        attributes = feature['properties']
+        arcgis_feature = Feature(geometry=geometry, attributes=attributes)
+        features.append(arcgis_feature)
 
-    return arcgis_json
+    return FeatureSet(features)
 
-def proxy_request(arcgis_json):
+def proxy_request(arcgis_features):
     url = 'https://services6.arcgis.com/QHir1urgnGYroCLG/arcgis/rest/services/PG_versioneret_110624/FeatureServer/0/addFeatures'
-    headers = {'Content-Type': 'application/json'}
-    data = {'features': [arcgis_json]}
-    response = requests.post(url, headers=headers, json=data)
+    params = {
+        'f': 'json',  # Specify JSON format
+    }
+    data = {
+        'features': arcgis_features.as_dict(),
+    }
+    response = requests.post(url, params=params, json=data)
     return response
 
 if __name__ == '__main__':
